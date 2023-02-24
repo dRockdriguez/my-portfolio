83b33e789a1fce71a66a496e17eab94e0f406339
David <david.rodriguez14@bbvaitspain.com>
2023-02-24 12:08:36 +0100

    feat: 🎸 changes

--- islands/Layout.tsx
+++ islands/Layout.tsx
@@ -12,30 +12,9 @@ export default function Layout(props) {
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
 
-    gsap.to(".vertical-scroll", {
+    ScrollTrigger.defaults({
       toggleActions: "restart pause resume pause",
-      scroller: ".vertical-scroll",
-      pin: true,
-      scrub: 1,
-    });
-
-    const sections = gsap.utils.toArray(".panel");
-    const horizontalScroll = document.querySelector(
-      ".horizontal-scroll",
-    ) as HTMLElement;
-
-    const offestWidth = horizontalScroll.offsetWidth;
-
-    gsap.to(sections, {
-      xPercent: -100 * (sections.length - 1),
-      ease: "none",
-      scrollTrigger: {
-        trigger: ".horizontal-scroll",
-        pin: true,
-        scrub: 1,
-        snap: 1 / (sections.length - 1),
-        end: () => "+=" + offestWidth,
-      },
+      scroller: ".layout-container",
     });
 
     setPreload(false);
@@ -43,25 +22,16 @@ export default function Layout(props) {
 
   return (
     <>
-      <div class={preload ? "vertical-scroll preload" : "vertical-scroll"}>
-        <Presentation></Presentation>
-      </div>
       <div
-        class={preload ? "horizontal-scroll preload" : "horizontal-scroll"}
+        class={preload ? "layout-container preload" : "layout-container"}
       >
-        <div class="panel" style="background:blue">
-          <AboutMe aboutme={aboutme}></AboutMe>
+        <div class="panel">
+          <Presentation></Presentation>
         </div>
 
-        <section class="panel">
-          ONE
-        </section>
-        <section class="panel">
-          TWO
-        </section>
-        <section class="panel">
-          THREE
-        </section>
+        <div class="panel panel-aboutme">
+          <AboutMe aboutme={aboutme}></AboutMe>
+        </div>
       </div>
     </>
   );
